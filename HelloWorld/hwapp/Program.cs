082b91c012b7a105cbd19a9b77ffabc3672e303d
ljxxcaijing <755763949@qq.com>
2017-09-14 00:10:23 +0800

    9.13Hw

---
+++ HelloWorld/hwapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Beep!");
+            Console.Beep(1000,1000);
+        }
+    }
+}
